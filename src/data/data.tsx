c30827efed1c345ab59ea020cb4fdea415b252f2
“Claudia_Gardner-Pickett” <claudia.gardnerpickett@sky.uk>
2022-03-22 21:00:08 +0000

    [chart]: add API data

---
+++ src/data/data.tsx
@@ -0,0 +1,245 @@
+import { ContentData } from "../types/contentData";
+
+// https://my-json-server.typicode.com/alb90/aieng-tech-test-assets/data
+export const contentData: ContentData[] = [
+  {
+    name: "Moana",
+    totalViews: {
+      total: 10919,
+      "sky-go": 1324,
+      "now-tv": 4213,
+      peacock: 5832,
+    },
+    prevTotalViews: {
+      total: 13687,
+      "sky-go": 1957,
+      "now-tv": 4181,
+      peacock: 7549,
+    },
+    description:
+      "To save her island home a plucky Polynesian teen must set sail and journey into the great blue yonder. Oscar-nominated Disney animation with the voice of Dwayne Johnson. (2016)(107 mins)",
+    duration: 6420,
+    assetImage:
+      "http://epgstatic.sky.com/epgdata/1.0/vodimage/0/0/Mas_moana.jpg",
+    videoImage:
+      "https://uk.imageservice.sky.com/contentid/764b39c2db1bc510VgnVCM1000000b43150a____/BOXART",
+    provider: "Sky Disney",
+    genre: ["kids", "movies", "animation"],
+  },
+  {
+    name: "Trolls",
+    totalViews: {
+      total: 10266,
+      "sky-go": 4853,
+      "now-tv": 1493,
+      peacock: 3920,
+    },
+    prevTotalViews: {
+      total: 11904,
+      "sky-go": 6344,
+      "now-tv": 898,
+      peacock: 4662,
+    },
+    description:
+      "A pair of fuzzy-headed trolls embark on a daring rescue mission when members of their tribe are kidnapped. Animation with the voices of Anna Kendrick and Justin Timberlake. (2016)(89 mins)",
+    duration: 5580,
+    assetImage:
+      "http://epgstatic.sky.com/epgdata/1.0/vodimage/0/0/Mas_trolls.jpg",
+    videoImage:
+      "https://uk.imageservice.sky.com/contentid/b77c1deb8ad0d510VgnVCM1000000b43150a____/BOXART",
+    provider: "Sky Cinema",
+    genre: ["kids", "movies", "animation"],
+  },
+  {
+    name: "The Boss Baby",
+    totalViews: {
+      total: 8888,
+      "sky-go": 3352,
+      "now-tv": 3512,
+      peacock: 2024,
+    },
+    prevTotalViews: {
+      total: 9833,
+      "sky-go": 5009,
+      "now-tv": 3588,
+      peacock: 1236,
+    },
+    description:
+      "Animated comedy about a youngster concerned that his baby brother is actually a power-hungry tycoon sent to destroy his perfect family life. With the voice of Alec Baldwin. (2017)(94 mins)",
+    duration: 5820,
+    assetImage:
+      "http://epgstatic.sky.com/epgdata/1.0/vodimage/0/0/Mas_thebossbaby.jpg",
+    videoImage:
+      "https://uk.imageservice.sky.com/contentid/246c96ac21a20610VgnVCM1000000b43150a____/BOXART",
+    provider: "Sky Cinema",
+    genre: ["kids", "movies", "animation"],
+  },
+  {
+    name: "Despicable Me 3",
+    totalViews: {
+      total: 8143,
+      "sky-go": 6059,
+      "now-tv": 0,
+      peacock: 2084,
+    },
+    prevTotalViews: {
+      total: 10581,
+      "sky-go": 8823,
+      "now-tv": 0,
+      peacock: 1758,
+    },
+    description:
+      "Dismissed from the Anti-Villain League after botching a mission, Gru sets off to meet his long-lost twin brother. Smash-hit animated sequel. (2017)(86 mins)",
+    duration: 5340,
+    assetImage:
+      "http://epgstatic.sky.com/epgdata/1.0/vodimage/0/0/Mas_despicableme3.jpg",
+    videoImage:
+      "https://uk.imageservice.sky.com/contentid/88350069442f1610VgnVCM1000000b43150a____/BOXART",
+    provider: "Sky Cinema",
+    genre: ["kids", "movies", "animation"],
+  },
+  {
+    name: "Frozen",
+    totalViews: {
+      total: 9384,
+      "sky-go": 3833,
+      "now-tv": 718,
+      peacock: 4833,
+    },
+    prevTotalViews: {
+      total: 7428,
+      "sky-go": 4233,
+      "now-tv": 420,
+      peacock: 2775,
+    },
+    description:
+      "A plucky princess heads into the frozen forest to search for her sister, whose powers have locked the kingdom in winter. Oscar-winning Disney animation with Kristen Bell. (2013)(98 mins)",
+    duration: 6120,
+    assetImage:
+      "http://epgstatic.sky.com/epgdata/1.0/vodimage/0/0/Mas_frozen.jpg",
+    videoImage:
+      "https://uk.imageservice.sky.com/contentid/a3786c0cacc5e510VgnVCM1000000b43150a____/BOXART",
+    provider: "Sky Disney",
+    genre: ["kids", "movies", "animation"],
+  },
+  {
+    name: "Sing",
+    totalViews: {
+      total: 10282,
+      "sky-go": 1947,
+      "now-tv": 4723,
+      peacock: 3612,
+    },
+    prevTotalViews: {
+      total: 9869,
+      "sky-go": 2331,
+      "now-tv": 4335,
+      peacock: 3203,
+    },
+    description:
+      "Family animation about a theatre-owning koala who holds a singing competition in a last-ditch bid to stay afloat. With the voices of Matthew McConaughey and John C Reilly. (2016)(103 mins)",
+    duration: 6420,
+    assetImage:
+      "http://epgstatic.sky.com/epgdata/1.0/vodimage/0/0/Mas_sing.jpg",
+    videoImage:
+      "https://uk.imageservice.sky.com/contentid/6c073db1c551f510VgnVCM1000000b43150a____/BOXART",
+    provider: "Sky Cinema",
+    genre: ["kids", "movies", "animation", "musical"],
+  },
+  {
+    name: "Baywatch",
+    totalViews: {
+      total: 16374,
+      "sky-go": 3482,
+      "now-tv": 1844,
+      peacock: 11048,
+    },
+    prevTotalViews: {
+      total: 15430,
+      "sky-go": 2342,
+      "now-tv": 2369,
+      peacock: 10719,
+    },
+    description:
+      "Beloved lifeguard chief Dwayne Johnson must team up with cocky new recruit Zac Efron to take down a drug-dealing villain. Action comedy. (2017)(112 mins)",
+    duration: 6960,
+    assetImage:
+      "http://epgstatic.sky.com/epgdata/1.0/vodimage/0/0/Mas_baywatch.jpg",
+    videoImage:
+      "https://uk.imageservice.sky.com/contentid/9697240164011610VgnVCM1000000b43150a____/BOXART",
+    provider: "Sky Cinema",
+    genre: ["comedy", "movies", "action"],
+  },
+  {
+    name: "Spider-Man: Homecoming",
+    totalViews: {
+      total: 6492,
+      "sky-go": 1203,
+      "now-tv": 3020,
+      peacock: 2269,
+    },
+    prevTotalViews: {
+      total: 7775,
+      "sky-go": 607,
+      "now-tv": 4224,
+      peacock: 2944,
+    },
+    description:
+      "Tom Holland is Peter Parker - an average teen with a web-slinging alter ego who must protect New York City from a menacing new villain. Superhero fun with Michael Keaton. (2017)(129 mins)",
+    duration: 7980,
+    assetImage:
+      "http://epgstatic.sky.com/epgdata/1.0/vodimage/0/0/Mas_spidermanhomecoming.jpg",
+    videoImage:
+      "https://uk.imageservice.sky.com/contentid/77e5b08ae2f02610VgnVCM1000000b43150a____/BOXART",
+    provider: "Sky Cinema",
+    genre: ["superhero", "movies", "action"],
+  },
+  {
+    name: "Pirates Of The Caribbean: Salazar's Revenge",
+    totalViews: {
+      total: 9282,
+      "sky-go": 5049,
+      "now-tv": 4233,
+      peacock: 0,
+    },
+    prevTotalViews: {
+      total: 10394,
+      "sky-go": 4067,
+      "now-tv": 6327,
+      peacock: 0,
+    },
+    description:
+      "...Salazar's Revenge: Swashbuckling high-seas adventure pitting Johnny Depp's Captain Jack Sparrow against undead pirate hunter Javier Bardem. With Geoffrey Rush. (2017)(124 mins)",
+    duration: 7740,
+    assetImage:
+      "http://epgstatic.sky.com/epgdata/1.0/vodimage/0/0/Mas_piratesofthecaribbean.jpg",
+    videoImage:
+      "https://uk.imageservice.sky.com/contentid/041971c6418d0610VgnVCM1000000b43150a____/BOXART",
+    provider: "Sky Cinema",
+    genre: ["movies", "action"],
+  },
+  {
+    name: "Monster Family (2017)",
+    totalViews: {
+      total: "3845",
+      "sky-go": "1103",
+      "now-tv": "233",
+      peacock: "3509",
+    },
+    prevTotalViews: {
+      total: "3748",
+      "sky-go": "1475",
+      "now-tv": "176",
+      peacock: "2097",
+    },
+    description:
+      "The first Sky Cinema Original Film sees a young family cursed by a witch and transformed into monsters. Animated comedy with the voices of Emily Watson and Jason Isaacs. (2017)(90 mins)",
+    duration: 5580,
+    assetImage:
+      "http://epgstatic.sky.com/epgdata/1.0/vodimage/0/0/Mas_monsterfamily.jpg",
+    videoImage:
+      "https://uk.imageservice.sky.com/contentid/b1595ae8164a1610VgnVCM1000000b43150a____/BOXART",
+    provider: "Sky Cinema",
+    genre: ["kids", "movies", "animation"],
+  },
+];
